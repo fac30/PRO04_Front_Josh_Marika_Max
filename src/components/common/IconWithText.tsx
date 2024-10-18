eab6f0c7ae9bff568f7cd1b3a77c531e26249b84
MarikaBBB <mbertelli012@gmail.com>
2024-10-18 18:45:07 +0100

    feat(accessibility): improve accessibility in IconWithText and Logo

--- src/components/common/IconWithText.tsx
+++ src/components/common/IconWithText.tsx
@@ -1,15 +1,20 @@
 type IconWithTextProps = {
     IconComponent: React.FC<{ size: number }>;
     label: string;
+    onClick?: () => void;
   };
 
 
-  const IconWithText = ({ IconComponent, label }: IconWithTextProps) => {
+  const IconWithText = ({ IconComponent, label, onClick }: IconWithTextProps) => {
     return (
-      <div className="flex items-center space-x-2 hover:text-gray-600">
-        <IconComponent size={24} />
+         <button
+         type="button"
+         onClick={onClick}
+         className="flex items-center space-x-2 hover:text-gray-600 focus:text-gray-600"
+         >
+        <IconComponent size={24} aria-hidden="true" />
         <span>{label}</span>
-      </div>
+      </button>
     );
   };
 
--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -6,8 +6,8 @@ import vinylLog from '../../assets/vinylLog.jpg';
 const Logo = () => {
     return (
         <Link to="/" className="flex items-center space-x-2" aria-label="Go to about page">  
-        <img src={vinylLog} alt=" " className="h-10 w-10" /> 
-        <h1 className="text-xl font-bold">Font Hill Records</h1>
+          <img src={vinylLog} alt="Font Hill Record" className="h-10 w-10" /> 
+          <h1 className="text-xl font-bold">Font Hill Records</h1>
         </Link>
       )
 }
