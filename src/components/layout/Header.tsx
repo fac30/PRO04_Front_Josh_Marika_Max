f6cb2459e622849faa057e808f75aa5995a7afb5
MarikaBBB <mbertelli012@gmail.com>
2024-10-18 11:51:47 +0100

    feat: create NavBar component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import Logo from "../common/Logo";
+import NavBar from "../layout/NavBar"
 import { FaUserCircle, FaShoppingBag } from "react-icons/fa";
 
 
@@ -8,10 +9,16 @@ const Header = () => {
       <div className="flex justify-between items-center p-4">
         <Logo aria-label="Font Hill Records logo" />
         <div className="flex items-center space-x-4">
-          <FaUserCircle size={24} className="hover:text-gray-600" />
-          <FaShoppingBag size={24} className="hover:text-gray-600" />
+        <div className="flex items-center space-x-2 hover:text-gray-600">
+          <FaUserCircle size={24} />
+          <span>Account</span>
+          </div>
+          <div className="flex items-center space-x-2 hover:text-gray-600"></div>
+          <FaShoppingBag size={24} />
+          <span>Cart</span>
         </div>
       </div>
+      <NavBar />
       <div className="bg-gray-100 text-center py-2 font-semibold">
         FREE UK DELIVERY ON ALL ORDERS (International delivery available)
       </div>
