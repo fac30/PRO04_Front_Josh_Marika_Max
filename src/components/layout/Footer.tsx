de1421eba85f49a0db0e26767390b31964e70d48
MarikaBBB <mbertelli012@gmail.com>
2024-10-20 11:29:07 +0100

    refactor(footer): remove unnecessary div

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,9 +4,8 @@ const Footer = () => {
       role="contentinfo"
       className="bg-background-footer text-text-primary py-8 w-full"
     >
-      <div className="text-center">
-        <p>&copy; 2024 Font Hill Records. All rights reserved.</p>  
-        </div>
+        <p className="text-center">&copy; 2024 Font Hill Records. All rights reserved.</p>  
+      
       </footer>
     );
   };
