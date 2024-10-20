26722bb6a19e17856e1fa6e5a5555a7cba4527a8
MarikaBBB <mbertelli012@gmail.com>
2024-10-20 11:42:12 +0100

    refactor(homepage): remove unnecessary main wrapper

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,10 +2,8 @@ import Main from "../components/layout/Main";
 
 const HomePage = () => {
   return (
-    // <main className="min-h-screen bg-background-light w-full flex flex-col">
      
       <Main />
-    // </main>  
   );
 };
 
