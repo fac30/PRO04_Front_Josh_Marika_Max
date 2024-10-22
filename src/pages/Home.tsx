59f1a71503236aab0ffcee6cef723bbfb3ffb506
JoshCodedit <joshua.dixoncms@gmail.com>
2024-10-22 15:11:37 +0100

    feat: added staff picks section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,8 @@ interface Product {
 }
 
 const Home = () => {
-  const [productData, setProductData] = useState<Product[]>([]);
+  const [latestReleases, setLatestReleases] = useState<Product[]>([]);
+  const [staffPicks, setStaffPicks] = useState<Product[]>([]);
   const [cartCount, setCartCount] = useState(0);
 
   useEffect(() => {
@@ -20,16 +21,19 @@ const Home = () => {
       try {
         const data: Product[] = await fetchData("vinyls", "GET");
 
-        // Sort by release_date in descending order (newest first) and take the top 8
-        const sortedData = data
+        const sortedLatest = data
           .sort(
             (a, b) =>
               new Date(b.release_date).getTime() -
               new Date(a.release_date).getTime(),
           )
-          .slice(0, 8); // Limit to 8 newest albums
+          .slice(0, 8);
 
-        setProductData(sortedData);
+        setLatestReleases(sortedLatest);
+
+        const shuffledVinyls = data.sort(() => 0.5 - Math.random()).slice(0, 4);
+
+        setStaffPicks(shuffledVinyls);
       } catch (error) {
         console.error("Error fetching product data:", error);
       }
@@ -48,7 +52,7 @@ const Home = () => {
       className="items-center justify-center text-center px-4 py-4 flex flex-col"
       role="main"
     >
-      <h2 className="text-4xl font-bold mb-4 text-text-primary">
+      <h2 className="text-4xl font-bold mb-4 mt-11 text-text-primary">
         Welcome to Font Hill Records
       </h2>
       <p className="text-xl mb-6 text-text-secondary">
@@ -58,12 +62,26 @@ const Home = () => {
       <section className="mb-12 max-w-90" aria-labelledby="new-on-store">
         <h3
           id="new-on-store"
-          className="text-2xl font-semibold mb-4 text-text-primary"
+          className="text-2xl font-semibold mb-14 text-text-primary"
         >
-          Latest Releases
+          Latest Releases:
         </h3>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-          {productData.map((product) => (
+          {latestReleases.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+
+      <section className="mb-12 max-w-90" aria-labelledby="staff-picks">
+        <h3
+          id="staff-picks"
+          className="text-2xl font-semibold mb-14 mt-28 text-text-primary"
+        >
+          Staff's Picks:
+        </h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+          {staffPicks.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
