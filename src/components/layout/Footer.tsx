f49ce64671b52c8b237d73c12d088fa4639412ed
MarikaBBB <mbertelli012@gmail.com>
2024-10-23 23:07:01 +0100

    style: change text colour in the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
       <footer
       role="contentinfo"
          className="bg-background-footer text-text-primary py-8 w-full">
-        <p className="text-center">© 2024 Font Hill Records. All rights reserved.</p>
+        <p className="text-center text-white">© 2024 Font Hill Records. All rights reserved.</p>
         </footer>
 
     );
