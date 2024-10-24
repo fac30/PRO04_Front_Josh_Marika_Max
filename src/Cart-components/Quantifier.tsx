65d1611a483287da4901b307f2670cc039caf77b
MarikaBBB <mbertelli012@gmail.com>
2024-10-24 11:19:49 +0100

    fix(Qunatifier): remuved duplicate text-white

--- src/Cart-components/Quantifier.tsx
+++ src/Cart-components/Quantifier.tsx
@@ -10,13 +10,13 @@ const Quantifier = ({quantity, onIncrease, onDecrease}: QuantifierProps) => {
         id="quantifier"
         className=" flex items-center space-x-0 border border-gray shadow-md">
             <button
-            className="rounded text-white font-bold bg-background-footer text-white hover:bg-accent-light transition w-12 h-12 flex justify-center items-center" 
+            className="rounded text-white font-bold bg-background-footer hover:bg-accent-light transition w-12 h-12 flex justify-center items-center" 
             onClick={onDecrease}>
                 -
             </button>
             <span className="rounded text-lg bg-white w-12 h-12 flex justify-center items-center">{quantity}</span>
             <button 
-            className="rounded text-white font-bold bg-background-footer text-white hover:bg-accent-light transition w-12 h-12 flex justify-center items-center"  
+            className="rounded text-white font-bold bg-background-footer hover:bg-accent-light transition w-12 h-12 flex justify-center items-center"  
             onClick={onIncrease}>
                 +
             </button>
