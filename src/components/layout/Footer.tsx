a8939b88e175685257670d74edd2bc2d90edd16d
Marika Bertelli <126022615+MarikaBBB@users.noreply.github.com>
2024-10-24 11:16:31 +0100

    Update src/components/layout/Footer.tsx
    Co-authored-by: Maxime Downe <81163597+maxitect@users.noreply.github.com>

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
       <footer
       role="contentinfo"
       className="bg-background-footer text-text-primary py-8 w-full">
-        <p className="text-center">© 2024 Font Hill Records. All rights reserved.</p>
+        <p className="text-center">&copy; 2024 Font Hill Records. All rights reserved.</p>
         </footer>
 
     );
