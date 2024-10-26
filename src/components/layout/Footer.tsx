24607624dce000fb19cdb4e59c88accc38e88459
MarikaBBB <mbertelli012@gmail.com>
2024-10-26 18:35:58 +0100

    feat(footer): move subscription form into footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,16 @@
+import NewsletterForm from "../NewsLetterForm";
+
 const Footer = () => {
     return (
       <footer
       role="contentinfo"
       className="bg-background-footer text-text-primary py-8 w-full">
+        
+        <div className="ml-auto max-w-md mr-8">
+        <NewsletterForm />
+      </div>
+
+
         <p className="text-center">&copy; 2024 Font Hill Records. All rights reserved.</p>
         </footer>
 
