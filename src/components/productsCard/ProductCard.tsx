3a431e677422cfb5d9195c72627007097bcb367c
MarikaBBB <mbertelli012@gmail.com>
2024-10-28 21:24:24 +0000

    feat(cart): implement add to cart functionality in ProductCard component

--- src/components/productsCard/ProductCard.tsx
+++ src/components/productsCard/ProductCard.tsx
@@ -1,13 +1,19 @@
 // src/components/productsCard/ProductCard.tsx
 
+import { useCartContext } from '../../Context/Cart';
 import { Vinyl } from "../../utils/types";
 
 interface ProductCardProps {
-  vinyl: Vinyl; // Changed to 'vinyl' to follow convention
-  addToCart: (vinyl: Vinyl) => void; // Include addToCart in the props
+  vinyl: Vinyl; 
 }
 
-const ProductCard = ({ vinyl, addToCart }: ProductCardProps) => {
+const ProductCard = ({ vinyl }: ProductCardProps) => {
+  const { dispatch } = useCartContext();
+
+  const handleAddToCart = () => {
+    dispatch({ type: 'ADD_TO_CART', payload: vinyl });
+  };
+
   return (
     <div className="bg-white shadow-md p-4 max-w-80 w-full">
       {vinyl.image_url ? (
@@ -27,7 +33,7 @@ const ProductCard = ({ vinyl, addToCart }: ProductCardProps) => {
         <p className="text-gray-800 font-semibold">£{vinyl.price}</p>
         <button
           className="mt-4 bg-background-light text-black py-2 px-4 rounded-lg hover:bg-background-footer transition"
-          onClick={() => addToCart(vinyl)} // Pass the vinyl object to addToCart
+          onClick={handleAddToCart}
         >
           Add To Basket
         </button>
