91a1921a810bc8803817613461f3785c9423153d
JoshCodedit <joshua.dixoncms@gmail.com>
2024-10-30 14:29:36 +0000

    style: footer styling

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer
       role="contentinfo"
-      className="bg-background-footer text-text-primary py-8 w-full"
+      className="bg-background-footer text-white py-8 w-full text-center"
     >
       <div className="flex flex-col justify-between items-center mx-auto max-w-6xl px-4">
         <div className="max-w-md w-full mb-11 md:mr-8">
@@ -13,7 +13,7 @@ const Footer = () => {
         </div>
 
         {/* Socials */}
-        <div className="mb-4 md:mb-0">
+        <div className="mb-4 md:mb-0 ">
           <Socials />
         </div>
       </div>
